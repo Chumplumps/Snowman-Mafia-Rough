5136abe5d1f580026b18892822e27064115fedc9
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-22 23:04:42 +1000

    fix snowmen
    they act a lil odd but i think it is balanced

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     [Header("Spawners")]
     public GameObject[] snowmanWaypoint;
     //snowballs spawn at snowmen transform.position
-    public GameObject[] snowMen;
+  
     [Header("Prefabs")]
     public GameObject snowBall;
     public GameObject snowMan;
@@ -15,14 +15,16 @@ public class SpawnManager : MonoBehaviour
     public int snowmanNum;
     public float snowballTimer;
     public float snowmanTimer;
+    private GameObject[] newSnowMan = new GameObject[3];
     // Start is called before the first frame update
+    
     void Start()
     {
         snowmanWaypoint[0] = GameObject.Find("WaypointLeft");
         snowmanWaypoint[1] = GameObject.Find("WaypointMiddle");
         snowmanWaypoint[2] = GameObject.Find("WaypointRight");
 
-
+       
 
 
     }
@@ -30,16 +32,21 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+           
+        
+       
         //timer for snowman spawn
         snowmanTimer += Time.deltaTime;
-        if (snowmanTimer > 3)
+        if (snowmanTimer > 4)
         {
             SnowmanSpawner();
             snowmanTimer = 0;
         }
         //timer for snowball spawn
         snowballTimer += Time.deltaTime;
-        if (snowballTimer > 3)
+        if (snowballTimer > 1)
         {
             SnowballSpawner();
             snowballTimer = 0;
@@ -48,34 +55,15 @@ public class SpawnManager : MonoBehaviour
 
 
     }
+   
     public void SnowmanSpawner()
     {
         waypointNum = Random.Range(0, 3);
-        Instantiate(snowMan, snowmanWaypoint[waypointNum].transform.position, Quaternion.identity);
-
+        
+       newSnowMan[waypointNum]= Instantiate(snowMan, snowmanWaypoint[waypointNum].transform.position, Quaternion.identity);
+       
         Debug.Log("if you got to here you are doing kinda good");
-        //rename prefab in scene based on which waypoint it spawns at
-        if (waypointNum == 1)
-        {
-            snowMan.name = "Snowman1";
 
-        }
-        if (waypointNum == 2)
-        {
-            snowMan.name = "Snowman2";
-
-        }
-        if (waypointNum == 3)
-        {
-            snowMan.name = "Snowman3";
-
-        }
-
-
-        //array snowman = gameobject in scene
-        snowMen[0] = GameObject.Find("Snowman1(Clone)");
-        snowMen[1] = GameObject.Find("Snowman2(Clone)");
-        snowMen[2] = GameObject.Find("Snowman3(Clone)");
     }
     public void SnowballSpawner()
     {
@@ -83,33 +71,12 @@ public class SpawnManager : MonoBehaviour
 
         snowmanNum = Random.Range(0, 3);
         Vector3 spawnPos = new Vector3(0, 0, -3);
-        //if (snowMen[0] == null)
-        //{
-        //    snowmanNum = Random.Range(2, 3);
-        //}
-        //if (snowMen[1] == null)
-        //{
-        //    if (Random.value<.5f)
-        //    {
-        //        snowmanNum = 1;
-        //    }
-        //    else
-        //    {
-        //        snowmanNum = 2;
-        //    }
-        //}
-        //if (snowMen[2] == null)
-        //{
-        //    if (Random.value<.5f)
-        //    {
-        //        snowmanNum = 1;
-        //    }
-        //    else
-        //    {
-        //        snowmanNum = 3;
-        //    }
-        //}
-        Instantiate(snowBall, snowMen[snowmanNum].transform.position + spawnPos, Quaternion.identity);
+        if (newSnowMan[snowmanNum] != null && !newSnowMan[snowmanNum].Equals(null))
+        {
+            Instantiate(snowBall, newSnowMan[snowmanNum].transform.position + spawnPos, Quaternion.identity);
+
+        }
+     
 
 
     }
